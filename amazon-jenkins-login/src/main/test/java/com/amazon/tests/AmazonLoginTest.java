b2a41b13d010b7589518b3165d3dcbe7416f1f9e
akshitrajpatel <aksh.patel78@gmail.com>
2024-08-27 22:20:13 +0530

    initial commit

---
+++ amazon-jenkins-login/src/main/test/java/com/amazon/tests/AmazonLoginTest.java
@@ -0,0 +1,40 @@
+package com.amazon.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AmazonLoginTest {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.amazon.com/ap/signin");
+    }
+
+    @Test
+    public void testLogin() {
+        driver.findElement(By.id("ap_email")).sendKeys("your-email@example.com");
+        driver.findElement(By.id("continue")).click();
+        driver.findElement(By.id("ap_password")).sendKeys("your-password");
+        driver.findElement(By.id("signInSubmit")).click();
+
+        // Assert the login success by checking some element on the home page
+        String title = driver.getTitle();
+        assert title.contains("Amazon.com");
+    }
+
+    @AfterClass
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
